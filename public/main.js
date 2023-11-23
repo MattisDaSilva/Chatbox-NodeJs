9d894051b846d8ffec7c4d78f13e554cd851d349
Mattis Da Silva <52781802+MattisDaSilva@users.noreply.github.com>
2023-11-23 15:36:21 +0100

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 function submitForm(event) {
-    event.preventDefault();
-    const inputValue = document.getElementById('inputName').value;
-    window.location.href = `/chat.html?inputValue=${inputValue}`;
-  }
+  event.preventDefault();
+  const inputValue = document.getElementById('inputName').value;
+  window.location.href = `/chat.html?inputValue=${inputValue}`;
+}
+
