f970fea93a9ad52ba8616f4a67e8a11289414b1b
EvannBoistuaud <evann.boistuaud@gmail.com>
2023-11-23 11:04:41 +0100

    Message
    Système de message fonctionnel

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = require('socket.io')(server);
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
@@ -14,35 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 
 // Define a route to serve the HTML page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-// Route pour gérer le formulaire soumis
-app.post('/submit', (req, res) => {
-  const inputValue = req.body.inputName; // inputName est le nom de votre champ d'entrée dans le formulaire
-  console.log('Valeur de l\'input:', inputValue);
-  // Faites ce que vous voulez avec la valeur de l'input ici
-  res.send('Données reçues avec succès.');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    socket.on('send name', (username) => {
+        io.emit('send name', username);
+    });
 
-  // Handle chat messages
-  socket.on('chat message', (msg) => {
-    console.log('Message:', msg);
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
-  });
+    socket.on('send message', (chat) => {
+        io.emit('send message', chat);
+    });
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
